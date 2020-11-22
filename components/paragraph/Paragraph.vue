ec57234255e467040c265e88ab82ceb32a4037f0
marcus <marcusmmourao@gmail.com>
2020-11-22 18:25:32 -0300

    feat: implement paragraph component

---
+++ components/paragraph/Paragraph.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'MmParagraph',
+  functional: true,
+  render(createElement, { children, data }) {
+    return createElement('p', {
+      class: 'mm-paragraph',
+      ...data,
+    }, children);
+  },
+};
+</script>
+
+<style lang="css" scoped>
+  @import "./paragraph.css";
+</style>
