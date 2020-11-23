ff0e389c7e6701b6a6fd7c8aad8f0dd17b256044
marcus <marcusmmourao@gmail.com>
2020-11-22 21:50:04 -0300

    feat: implement card component

---
+++ components/card/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mm-card">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MmCard',
+};
+</script>
+
+<style lang="css" scoped>
+  @import "./card.css";
+</style>
