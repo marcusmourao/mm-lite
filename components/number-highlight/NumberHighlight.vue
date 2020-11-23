de09a5e3bee9d199aa549a811a895059573761df
marcus <marcusmmourao@gmail.com>
2020-11-22 22:12:07 -0300

    feat: implement number highlight component

---
+++ components/number-highlight/NumberHighlight.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: 'MmNumberHighlight',
+  functional: true,
+  props: {
+    theme: {
+      type: String,
+      default: null,
+      validator(value) {
+        return !value || ['success', 'alert'].includes(value);
+      },
+    },
+    value: {
+      type: Number,
+      required: true,
+    },
+  },
+  render(createElement, { props }) {
+    return createElement('span', {
+      class: {
+        'mm-number-highlight': true,
+        [`mm-number-highlight--${props.theme}`]: props.theme,
+      },
+    }, props.value);
+  },
+};
+</script>
+
+<style lang="css" scoped>
+  @import "./numberHighlight.css";
+</style>
---
+++ components/number-highlight/index.js
@@ -0,0 +1,1 @@
+export { default } from './NumberHighlight.vue';
