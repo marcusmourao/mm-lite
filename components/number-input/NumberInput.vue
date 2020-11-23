4f15fb2c34e7f0a69a2da8e73168a581b8685ab9
marcus <marcusmmourao@gmail.com>
2020-11-22 23:17:48 -0300

    feat: implement number input

---
+++ components/number-input/NumberInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <mm-input
+    :value="value"
+    v-bind="$attrs"
+    type="number"
+    :custom-validations="customValidations"
+    @input="onInput"
+  />
+</template>
+
+<script>
+import MmInput from '../input';
+
+export default {
+  name: 'MmNumberInput',
+  components: { MmInput },
+  props: {
+    customValidations: {
+      type: Array,
+      default: () => [],
+    },
+    value: {
+      type: [String, Number],
+      default: null,
+    },
+  },
+  methods: {
+    onInput(value) {
+      this.$emit('input', parseInt(value, 10));
+    },
+  },
+};
+</script>
